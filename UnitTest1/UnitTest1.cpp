f0e61471ef6b81859002f11ff971412042d0ce42
nimrodTau2017 <nimrodse@post.tau.ac.il>
2026-03-09 09:35:38 +0200

    with tree_from_newick

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -6,11 +6,16 @@
 #include "../sp_alternative_cpp/include/sp_score.h"
 #include <numeric>
 #include "../sp_alternative_cpp/include/distance_calc.h"
+#include "../sp_alternative_cpp/include/node.h"
+#include "../sp_alternative_cpp/include/unrooted_tree.h"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
 
 namespace spalternativeUnitTests
 {
+
+	string newick_of_AATF = "((((Macropus:0.051803,Monodelphis:0.066021):0.016682,Sarcophilus:0.068964):0.114355,((Echinops:0.104144,(Loxodonta:0.076474,Procavia:0.076193):0.011550):0.013015,((Choloepus:0.056091,Dasypus:0.040600):0.013681,(((((Callithrix:0.032131,((((Gorilla:0.007042,(Homo:0.002445,Pan:0.002450):0.001237):0.003689,Pongo:0.007508):0.002384,Nomascus:0.015696):0.004674,Macaca:0.013752):0.012205):0.029730,((Microcebus:0.037066,Otolemur:0.050935):0.008091,Tarsius:0.064938):0.007305):0.003377,Tupaia:0.090946):0.000699,(((Cavia:0.116826,(Dipodomys:0.080386,(Mus:0.040313,Rattus:0.033872):0.122329):0.013314):0.000298,Ictidomys:0.062932):0.011064,(Ochotona:0.087746,Oryctolagus:0.057769):0.035947):0.002130):0.006931,(Erinaceus:0.094727,(((((Bos:0.062659,Tursiops:0.024374):0.009782,Sus:0.064336):0.006134,Vicugna:0.049961):0.021643,Equus:0.046559):0.002728,(Sorex:0.126677,((Myotis:0.050452,Pteropus:0.047740):0.006495,(Felis:0.042414,(Canis:0.036675,(Mustela:0.027691,Ailuropoda:0.037553):0.004251):0.008141):0.019485):0.000485):0.003533):0.000528):0.005645):0.012900):0.002853):0.133251):0.019558,Ornithorhynchus:0.195576);";
+
 	TEST_CLASS(spalternativeUnitTests)
 	{
 	public:
@@ -638,6 +643,174 @@ namespace spalternativeUnitTests
 
 			double res = compute_distance(profileB, profileA, DistanceType::D_POS);
 			Assert::AreEqual(res, 0.5);
+		}
+
+		TEST_METHOD(tree_from_newick)
+		{
+			std::pair<Node*, std::vector<Node*>> res = create_a_tree_from_newick(newick_of_AATF);
+			std::unordered_map<int, std::set<std::string>> expextedTaxaDict;
+			std::unordered_map<int, std::vector<int>> expextedChildrenIdsDict;
+
+			expextedChildrenIdsDict[78] = {76, 77};
+			expextedChildrenIdsDict[76] = {4, 75};
+			expextedTaxaDict[76] = {
+				"Ailuropoda", "Bos", "Callithrix", "Canis", "Cavia", "Choloepus", "Dasypus", "Dipodomys", "Echinops",
+				"Equus", "Erinaceus", "Felis", "Gorilla", "Homo", "Ictidomys", "Loxodonta", "Macaca", "Macropus",
+				"Microcebus", "Monodelphis", "Mus", "Mustela", "Myotis", "Nomascus", "Ochotona", "Oryctolagus", "Otolemur",	"Pan", 
+				"Pongo", "Procavia", "Pteropus", "Rattus", "Sarcophilus", "Sorex", "Sus", "Tarsius", "Tupaia", "Tursiops", "Vicugna" };
+			expextedChildrenIdsDict[75] = { 9, 74 };
+			expextedTaxaDict[75] = {
+				"Ailuropoda", "Bos", "Callithrix", "Canis", "Cavia", "Choloepus", "Dasypus", "Dipodomys", "Echinops",
+				"Equus", "Erinaceus", "Felis", "Gorilla", "Homo", "Ictidomys", "Loxodonta", "Macaca", "Microcebus",
+				"Mus", "Mustela", "Myotis", "Nomascus", "Ochotona", "Oryctolagus", "Otolemur", "Pan", "Pongo", "Procavia",
+				"Pteropus", "Rattus", "Sorex", "Sus", "Tarsius", "Tupaia", "Tursiops", "Vicugna" };
+			expextedChildrenIdsDict[74] = { 12, 73 };
+			expextedTaxaDict[74] = {
+				"Ailuropoda", "Bos", "Callithrix", "Canis", "Cavia", "Choloepus", "Dasypus", "Dipodomys", "Equus","Erinaceus", "Felis", "Gorilla", 
+				"Homo", "Ictidomys", "Macaca", "Microcebus", "Mus", "Mustela", "Myotis", "Nomascus", "Ochotona", "Oryctolagus", "Otolemur", "Pan", 
+				"Pongo", "Pteropus", "Rattus", "Sorex", "Sus", "Tarsius", "Tupaia", "Tursiops", "Vicugna" };
+			expextedChildrenIdsDict[73] = { 47, 72 };
+			expextedTaxaDict[73] = { "Ailuropoda", "Bos", "Callithrix", "Canis", "Cavia", "Dipodomys", "Equus", "Erinaceus", "Felis", "Gorilla",
+				"Homo", "Ictidomys", "Macaca", "Microcebus", "Mus", "Mustela", "Myotis", "Nomascus", "Ochotona", "Oryctolagus", "Otolemur", "Pan", "Pongo", 
+				"Pteropus", "Rattus", "Sorex", "Sus", "Tarsius", "Tupaia", "Tursiops", "Vicugna" };
+			expextedChildrenIdsDict[47] = { 33, 46 };
+			expextedTaxaDict[47] = { "Callithrix", "Cavia", "Dipodomys", "Gorilla", "Homo", "Ictidomys", "Macaca", "Microcebus", "Mus",
+				"Nomascus", "Ochotona", "Oryctolagus", "Otolemur", "Pan", "Pongo", "Rattus", "Tarsius", "Tupaia" };
+			expextedChildrenIdsDict[72] = { 48, 71 };
+			expextedTaxaDict[72] = { "Ailuropoda", "Bos", "Canis", "Equus", "Erinaceus", "Felis", "Mustela", "Myotis", "Pteropus", "Sorex",
+				"Sus", "Tursiops", "Vicugna" };
+			expextedChildrenIdsDict[71] = { 57, 70 };
+			expextedTaxaDict[71] = { "Ailuropoda", "Bos", "Canis", "Equus", "Felis", "Mustela", "Myotis", "Pteropus", "Sorex", "Sus", "Tursiops", "Vicugna" };
+			expextedChildrenIdsDict[33] = { 31, 32 };
+			expextedTaxaDict[33] = { "Callithrix", "Gorilla", "Homo", "Macaca", "Microcebus", "Nomascus", "Otolemur", "Pan", "Pongo", "Tarsius", "Tupaia" };
+			expextedChildrenIdsDict[31] = { 25, 30 };
+			expextedTaxaDict[31] = { "Callithrix", "Gorilla", "Homo", "Macaca", "Microcebus", "Nomascus", "Otolemur", "Pan", "Pongo", "Tarsius" };
+			expextedChildrenIdsDict[70] = { 58, 69 };
+			expextedTaxaDict[70] = { "Ailuropoda", "Canis", "Felis", "Mustela", "Myotis", "Pteropus", "Sorex" };
+			expextedChildrenIdsDict[46] = { 42, 45 };
+			expextedTaxaDict[46] = { "Cavia", "Dipodomys", "Ictidomys", "Mus", "Ochotona", "Oryctolagus", "Rattus" };
+			expextedChildrenIdsDict[25] = { 13, 24 };
+			expextedTaxaDict[25] = { "Callithrix", "Gorilla", "Homo", "Macaca", "Nomascus", "Pan", "Pongo" };
+			expextedChildrenIdsDict[69] = { 61, 68 };
+			expextedTaxaDict[69] = { "Pteropus", "Mustela", "Canis", "Felis", "Ailuropoda", "Myotis" };
+			expextedChildrenIdsDict[24] = { 22, 23 };
+			expextedTaxaDict[24] = { "Pan", "Gorilla", "Macaca", "Homo", "Pongo", "Nomascus" };
+			expextedChildrenIdsDict[57] = { 55, 56 };
+			expextedTaxaDict[57] = { "Vicugna", "Sus", "Equus", "Bos", "Tursiops" };
+			expextedChildrenIdsDict[42] = { 40, 41 };
+			expextedTaxaDict[42] = { "Cavia", "Mus", "Dipodomys", "Ictidomys", "Rattus" };
+			expextedChildrenIdsDict[22] = { 20, 21 };
+			expextedTaxaDict[22] = { "Pan", "Gorilla", "Homo", "Pongo", "Nomascus" };
+			expextedChildrenIdsDict[68] = { 62, 67 };
+			expextedTaxaDict[68] = { "Canis", "Felis", "Mustela", "Ailuropoda" };
+			expextedChildrenIdsDict[55] = { 53, 54 };
+			expextedTaxaDict[55] = { "Sus", "Vicugna", "Bos", "Tursiops" };
+			expextedChildrenIdsDict[40] = { 34, 39 };
+			expextedTaxaDict[40] = { "Dipodomys", "Cavia", "Rattus", "Mus" };
+			expextedChildrenIdsDict[20] = { 18, 19 };
+			expextedTaxaDict[20] = { "Homo", "Pongo", "Pan", "Gorilla" };
+			expextedChildrenIdsDict[67] = { 63, 66 };
+			expextedTaxaDict[67] = { "Canis", "Mustela", "Ailuropoda" };
+			expextedChildrenIdsDict[53] = { 51, 52 };
+			expextedTaxaDict[53] = { "Sus", "Bos", "Tursiops" };
+			expextedChildrenIdsDict[39] = { 35, 38 };
+			expextedTaxaDict[39] = { "Dipodomys", "Rattus", "Mus" };
+			expextedChildrenIdsDict[30] = { 28, 29 };
+			expextedTaxaDict[30] = { "Microcebus", "Otolemur", "Tarsius" };
+			expextedChildrenIdsDict[18] = { 14, 17 };
+			expextedTaxaDict[18] = { "Homo", "Pan", "Gorilla" };
+			expextedChildrenIdsDict[9] = { 5 ,  8 };
+			expextedTaxaDict[9] = { "Procavia", "Loxodonta", "Echinops" };
+			expextedChildrenIdsDict[4] = { 2 ,  3 };
+			expextedTaxaDict[4] = { "Monodelphis", "Macropus", "Sarcophilus" };
+			expextedChildrenIdsDict[66] = { 64, 65 };
+			expextedTaxaDict[66] = { "Mustela", "Ailuropoda" };
+			expextedChildrenIdsDict[61] = { 59, 60 };
+			expextedTaxaDict[61] = { "Pteropus", "Myotis" };
+			expextedChildrenIdsDict[51] = { 49, 50 };
+			expextedTaxaDict[51] = { "Bos", "Tursiops" };
+			expextedChildrenIdsDict[45] = { 43, 44 };
+			expextedTaxaDict[45] = { "Oryctolagus", "Ochotona" };
+			expextedChildrenIdsDict[38] = { 36, 37 };
+			expextedTaxaDict[38] = { "Rattus", "Mus" };
+			expextedChildrenIdsDict[28] = { 26, 27 };
+			expextedTaxaDict[28] = { "Microcebus", "Otolemur" };
+			expextedChildrenIdsDict[17] = { 15, 16 };
+			expextedTaxaDict[17] = { "Homo", "Pan" };
+			expextedChildrenIdsDict[12] = { 10, 11 };
+			expextedTaxaDict[12] = { "Dasypus", "Choloepus" };
+			expextedChildrenIdsDict[8] = { 6, 7 };
+			expextedTaxaDict[8] = { "Procavia", "Loxodonta" };
+			expextedChildrenIdsDict[2] = { 0, 1 };
+			expextedTaxaDict[2] = { "Monodelphis", "Macropus" };
+			expextedTaxaDict[77] = { "Ornithorhynchus" };
+			expextedTaxaDict[65] = { "Ailuropoda" };
+			expextedTaxaDict[64] = { "Mustela" };
+			expextedTaxaDict[63] = { "Canis" };
+			expextedTaxaDict[62] = { "Felis" };
+			expextedTaxaDict[60] = { "Pteropus" };
+			expextedTaxaDict[59] = { "Myotis" };
+			expextedTaxaDict[58] = { "Sorex" };
+			expextedTaxaDict[56] = { "Equus" };
+			expextedTaxaDict[54] = { "Vicugna" };
+			expextedTaxaDict[52] = { "Sus" };
+			expextedTaxaDict[50] = { "Tursiops" };
+			expextedTaxaDict[49] = { "Bos" };
+			expextedTaxaDict[48] = { "Erinaceus" };
+			expextedTaxaDict[44] = { "Oryctolagus" };
+			expextedTaxaDict[43] = { "Ochotona" };
+			expextedTaxaDict[41] = { "Ictidomys" };
+			expextedTaxaDict[37] = { "Rattus" };
+			expextedTaxaDict[36] = { "Mus" };
+			expextedTaxaDict[35] = { "Dipodomys" };
+			expextedTaxaDict[34] = { "Cavia" };
+			expextedTaxaDict[32] = { "Tupaia" };
+			expextedTaxaDict[29] = { "Tarsius" };
+			expextedTaxaDict[27] = { "Otolemur" };
+			expextedTaxaDict[26] = { "Microcebus" };
+			expextedTaxaDict[23] = { "Macaca" };
+			expextedTaxaDict[21] = { "Nomascus" };
+			expextedTaxaDict[19] = { "Pongo" };
+			expextedTaxaDict[16] = { "Pan" };
+			expextedTaxaDict[15] = { "Homo" };
+			expextedTaxaDict[14] = { "Gorilla" };
+			expextedTaxaDict[13] = { "Callithrix" };
+			expextedTaxaDict[11] = { "Dasypus" };
+			expextedTaxaDict[10] = { "Choloepus" };
+			expextedTaxaDict[7] = { "Procavia" };
+			expextedTaxaDict[6] = { "Loxodonta" };
+			expextedTaxaDict[5] = { "Echinops" };
+			expextedTaxaDict[3] = { "Sarcophilus" };
+			expextedTaxaDict[1] = { "Monodelphis" };
+			expextedTaxaDict[0] = { "Macropus" };
+			vector<int> leafIds = { 77, 65, 64, 63, 62, 60, 59, 58, 56, 54, 52, 50, 49, 48, 44, 43, 41, 37, 36, 35, 34, 32, 29, 27, 26, 23, 21, 19, 16, 15, 
+				14, 13, 11, 10, 7 ,6 ,5 ,3 ,1 ,0 };
+			for (int i = 0; i < leafIds.size(); ++i)
+			{
+				expextedChildrenIdsDict[leafIds[i]] = {};
+			}
+
+			Assert::AreEqual(expextedChildrenIdsDict.size(), res.second.size());
+			for (int i = 0; i < expextedChildrenIdsDict.size(); ++i)
+			{
+				std::set<int> childrenIds;
+
+				for (Node* child : res.second[i]->children)
+				{
+					childrenIds.insert(child->id);
+				}
+
+				Assert::IsTrue(
+					std::equal(
+						expextedChildrenIdsDict[i].begin(), expextedChildrenIdsDict[i].end(),
+						childrenIds.begin(), childrenIds.end()
+					) && expextedChildrenIdsDict[i].size() == childrenIds.size()
+				);
+			}
+			for (int i = 0; i < expextedTaxaDict.size(); ++i)
+			{
+				Assert::IsTrue(expextedTaxaDict[i] == res.second[i]->keys);
+			}
 		}
 
 	};
